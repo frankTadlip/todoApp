151c9878fec5838bc352474b55129c8e72e550f6
Frank Tadlip <frank@bizbox.ph>
2017-04-27 10:00:19 +0800

    adding modules users, task and people

--- client/main.js
+++ client/main.js
@@ -3,6 +3,9 @@ import initContext from './configs/context';
 
 // modules
 import coreModule from './modules/core';
+import taskModule from './modules/task';
+import usersModule from './modules/users';
+import peopleModule from './modules/people';
 
 // init context
 const context = initContext();
@@ -10,4 +13,7 @@ const context = initContext();
 // create app
 const app = createApp(context);
 app.loadModule(coreModule);
+app.loadModule(taskModule);
+app.loadModule(usersModule);
+app.loadModule(peopleModule);
 app.init();
